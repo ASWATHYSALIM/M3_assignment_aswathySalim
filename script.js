146927a5d2ca12c674855dfa3f423d2577d140f0
ASWATHYSALIM <150260781+ASWATHYSALIM@users.noreply.github.com>
2024-01-21 19:14:10 +0530

    validation completeted upto account number,
    all validations completed, sign in form under work

--- script.js
+++ script.js
@@ -45,9 +45,6 @@ function valid_clNo()
             return false;
         }
     }
-
- 
-
     function valida_phNo() {
             // Get the phone number input value
             var pnNo = document.getElementById('phNo').value;
@@ -92,35 +89,95 @@ function valid_clNo()
           alert('Please enter a valid email address.');
           return false;
       }
-  }
-  function valid_brname() {
-    // Get the input value
-    var bname = document.getElementById('branchName').value;
-
-    // Define a regular expression for alphabets
-    var alphabetRegex = /^[A-Za-z]+$/;
-
-    // Test if the input consists only of alphabets
-    if (alphabetRegex.test(bname)) {
-        return true;
-    } else {
-        alert('Lbranch Name should contain only alphabets.');
-        return false;
     }
-}
+    function valid_brname() {
+        // Get the input value
+        var bname = document.getElementById('branchName').value;
+
+        // Define a regular expression for alphabets
+        var alphabetRegex = /^[A-Za-z]+$/;
+
+        // Test if the input consists only of alphabets
+        if (alphabetRegex.test(bname)) {
+            return true;
+        } else {
+            alert('Lbranch Name should contain only alphabets.');
+            return false;
+        }
+    }
+    function valid_IFS(){
+      var ifs = document.getElementById('ifsCode').value;
+
+      // Check if the string is exactly 12 characters long
+      if (ifs.length === 12) {
+          // Extract the first four characters
+          var four_ifs= ifs.substring(0, 4);
+
+          // Check if the first four characters are alphabetic
+          if (/^[A-Za-z]+$/.test(four_ifs)) {
+            // alert('The first four characters are alphabetic.');
+            return true
+          } else {
+              alert('IFS Code is not valid');
+              return false
+
+          }
+      } else {
+        alert('IFS Code is not valid');
+        return false
+      }
+    }
+    function valid_acc() {
+      // Get the pone number input value
+      var acc = document.getElementById('accNo').value;
+
+      // Define a regular expression for a 10-digit account number
+      var accountNumberRegex = /^\d{10}$/;
+
+      // Test if the input matches the pattern
+      if (accountNumberRegex.test(acc)) {
+         // alert('Valid phone number.');
+         return true;
+      } else {
+          alert('Please enter a valid 10-digit account number.');
+          return false;
+      }
+    }
+    function valid_reacc(){
+      var acc1=document.getElementById('accNo').value;
+      var reacc=document.getElementById('accRe').value;
+      if(reacc==acc1)
+      {
+        return true
+      }
+      else
+      {
+        alert('Account Number Mismatch');
+        return false          
+      }
+    } 
 
 
-    function valid_form()
+     function valid_form()
     {
         console.log("form validation")
-       if(valid_clNo()=== true && valid_fname()===true && valid_lname()===true && valida_phNo()===true && valid_reph()=== true && valid_Email()===true)   
+       if(valid_clNo()=== true && valid_fname()===true && valid_lname()===true && valida_phNo()===true && valid_reph()=== true && valid_Email()===true && valid_brname()==true && valid_IFS()==true &&valid_acc()==true && valid_reacc()==true) 
       {
+        alert("Signup Successfully completed Please SignIn");
        return true;
        }
        else 
        {
         return false;
        }
+    }
 
-
+    function valid_siginform()
+    {
+      if(valid_clNo()=== true)
+      {
+        alert("Sign In Successfully");
+        return true;
+      }
+      else return false;
     }
